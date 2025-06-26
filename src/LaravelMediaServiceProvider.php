0543aab686ea0f985fde2edc08185908b498d07c
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-06-26 18:29:02 +0330

    add'laravel-media' config and loaded migrations & config

--- src/LaravelMediaServiceProvider.php
+++ src/LaravelMediaServiceProvider.php
@@ -2,7 +2,18 @@
 
 namespace Theparyab\LaravelMedia;
 
+use Illuminate\Support\ServiceProvider;
+
 class LaravelMediaServiceProvider extends ServiceProvider
 {
+    public function register()
+    {
 
+    }
+
+    public function boot()
+    {
+            $this ->loadMigrationsFrom(__DIR__ .'/../migrations');
+            $this ->mergeConfigFrom(__DIR__ .'/../config/laravel-media.php','laravel-media');
+    }
 }
---
+++ src/Models/Media.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Theparyab\LaravelMedia\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Media extends Model
+{
+
+}
