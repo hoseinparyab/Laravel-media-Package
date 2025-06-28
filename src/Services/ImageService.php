3ebf8851e522725a14c0461d519d98dae3d0b637
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-06-28 18:16:17 +0330

    complete 'upload' function in Image Service

--- src/Services/ImageService.php
+++ src/Services/ImageService.php
@@ -3,12 +3,29 @@
 namespace Theparyab\LaravelMedia\Services;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Theparyab\LaravelMedia\Enums\MediaTypeEnum;
+use Theparyab\LaravelMedia\Models\Media;
 
 class ImageService
 {
-    public function upload(UploadedFile $file, string $filename, string $dir)
+    public function upload(UploadedFile $file, bool $isPrivate = false)
     {
-        //Todo
+        $filename = $file->getClientOriginalName();
+        //Upload file
+        Storage::putFileAs(  $path='images', $file,$filename .'-' .now());
+        //create a row for migration
+        Media::query()->create([
+            'user_id' => auth()->id(),
+            'filename' => $filename,
+            'path' => $path,
+            'is_private' => $isPrivate,
+            'files' =>[],
+            'type' =>MediaTypeEnum::TYPE_IMAGE->value,
+        ]);
 
+
+        //Return filename
+        return $filename;
     }
 }
