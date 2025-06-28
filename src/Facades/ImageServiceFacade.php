eb21369ed14b4ba5dd7181487516d93bdd0d3a89
Paryab Hosein <75984205+hoseinparyab@users.noreply.github.com>
2025-06-28 18:57:05 +0330

    add image service methods to related Facade

--- src/Facades/ImageServiceFacade.php
+++ src/Facades/ImageServiceFacade.php
@@ -2,8 +2,12 @@
 
 namespace Theparyab\LaravelMedia\Facades;
 
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static upload(UploadedFile $file, bool $isPrivate = false)
+ */
 class ImageServiceFacade extends Facade
 {
     protected  static  function  getFacadeAccessor()
